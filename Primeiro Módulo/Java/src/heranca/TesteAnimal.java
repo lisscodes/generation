2fc931ec87be41f071710bfb851ab4d47b01d15a
Lisandra Dias <92340440+lisscodes@users.noreply.github.com>
2022-04-07 20:09:27 -0300

    Primeiros exercícios com herança de classes.

---
+++ Primeiro Módulo/Java/src/heranca/TesteAnimal.java
@@ -0,0 +1,29 @@
+package heranca;
+
+public class TesteAnimal {
+	
+	public static void main(String[] args) {
+		
+	Cachorro testeCachorro = new Cachorro ("Xolinha;",5,"Latido (au au!!!!);",
+	"Sim;","Caramelo.");
+	Cavalo testeCavalo = new Cavalo ("Robson;",4,"Relinche;","Sim.","Pé-de-pano");
+	Preguica testePreguica = new Preguica("Preguicinha",25,"Silvo agudo.","Não;","Sim.");
+	
+	//saídas cachorro
+	
+	testeCachorro.imprimaSeparador();
+	testeCachorro.imprimirInfoDog();
+	
+	//saídas cavalo
+	
+	testeCavalo.imprimaSeparador();
+	testeCavalo.imprimirInfoCavalo();
+	
+	//saídas Preguiça
+	
+	testePreguica.imprimaSeparador();
+	testePreguica.imprimirInfoPreg();
+
+	}
+
+}
