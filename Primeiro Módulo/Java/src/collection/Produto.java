b67355805a07fb6e119c1d6e0265119578de0020
Lisandra Dias <92340440+lisscodes@users.noreply.github.com>
2022-04-12 23:34:38 -0300

    Add files via upload

--- Primeiro Módulo/Java/src/collection/Produto.java
+++ Primeiro Módulo/Java/src/collection/Produto.java
@@ -1,5 +1,58 @@
 package collection;
 
 public class Produto {
+	
+	private static long id;
+	private String nome;
+	private double preco;
+	private int quantidade;
+
+	public Produto() {
+
+	}
+
+	public Produto(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int total) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nNome: " + nome);
+		sb.append("\nPreço: R$ " + preco);
+		sb.append("\nQuantidade: " + quantidade);
+		
+		return sb.toString();
+	}
 
 }
